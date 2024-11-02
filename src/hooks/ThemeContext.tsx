a7d82b7e8c909321b375c50a4788ff87ed6a4ded
samanshaiza004 <samanshaiza@yahoo.com>
2024-11-02 04:34:19 +0000

    fixed theme togle button

--- src/hooks/ThemeContext.tsx
+++ src/hooks/ThemeContext.tsx
@@ -55,9 +55,9 @@ export const ThemeToggle = () => {
       className="w-9 h-9 rounded-full"
     >
       {theme === "dark" ? (
-        <Sun className="h-4 w-4" />
-      ) : (
         <Moon className="h-4 w-4" />
+      ) : (
+        <Sun className="h-4 w-4" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
