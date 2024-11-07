6da230e269fee29e8424a5de0a5c3cb9bc22128b
saman shaiza <samanshaiza@yahoo.com>
2024-11-07 17:44:53 -0600

    stupid bug with helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 // src/App.tsx
-import { Helmet } from "react-helmet-async";
+// import { Helmet } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import RootLayout from "./RootLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Helmet>
-        <html lang="en" />
-        <meta
-          name="description"
-          content="Personal website of Saman Shaiza, software developer"
-        />
-      </Helmet>
       <RootLayout />
     </BrowserRouter>
   );
