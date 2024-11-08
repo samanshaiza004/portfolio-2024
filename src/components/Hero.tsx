bcb43a11f1256a40dc56d874ec937a9925ef7971
saman shaiza <samanshaiza@yahoo.com>
2024-11-08 11:49:25 -0600

    added robots.txt and fixed up index.html

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -129,7 +129,6 @@ export function Hero({
 
                 <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white dark:border-gray-800 shadow-xl">
                   <img
-                    loading="lazy"
                     src={imageUrl}
                     alt={`${name}'s profile`}
                     className="w-full h-full object-cover"
